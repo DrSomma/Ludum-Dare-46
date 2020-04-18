9ff73f2e2d939d08ed100b7980283fdf0ac2be17
3graba <sgraba@web.de>
2020-04-18 17:47:47 +0200

    Cough

--- src/LudumDare46/Assets/Scripts/Virus.cs
+++ src/LudumDare46/Assets/Scripts/Virus.cs
@@ -9,6 +9,9 @@ public class Virus : MonoBehaviour
     public float range = 2f;
     public float percentSpread = 0.30f;
     public float infectionCycle = 2f; //Check every X-Sec
+    public float coughCycle = 2f; //Check every X-Sec
+    public float percentCough = 0.30f;
+    public GameObject cloud;
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +23,26 @@ public class Virus : MonoBehaviour
         isActiv = status;
         if(status){
             InvokeRepeating("InfectHumans", infectionCycle, infectionCycle);  //delay, repeat every
+            if(percentCough > 0)
+                InvokeRepeating("doCough", coughCycle, coughCycle);  //delay, repeat every
         }else{
             CancelInvoke();
         }
     }
 
-    // Update is called once per frame
+    void doCough(){
+        if(Random.Range(0f,1f)<=percentCough){
+            GameObject obj = Instantiate(cloud);
+            obj.transform.position = transform.position;
+        }
+    }
+
+
     void InfectHumans()
     {
         List<HumanProperties> allHumans = InfectionManager.Instance.getAllHumans();
         foreach (var human in allHumans)
         {
-            
             if(human.status == HealthStatusEnum.healthy){
                 if(Vector2.Distance(transform.position,human.transform.position) <= range){
                     if(Random.Range(0f,1f)<=percentSpread){
