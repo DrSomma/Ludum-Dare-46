b917a0ca37ff5ce3fc842c264a5ab8ddd9696ea4
Orpartlu <marvin.somma@web.de>
2020-04-18 05:06:18 +0200

    added MenschenEigenschaften & Enums

---
+++ src/LudumDare46/Assets/Scripts/Menschen/MenschEigenschaften.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenschEigenschaften : MonoBehaviour
+{
+
+    public MenschStatusEnum Status;
+    public GeschlechtEnum Geschlecht;
+    public int Alter;
+
+}
