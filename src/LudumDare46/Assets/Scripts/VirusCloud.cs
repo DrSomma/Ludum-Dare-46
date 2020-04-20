5e59a60d74f3bdf424feeb5beeeced800574641d
3graba <sgraba@web.de>
2020-04-20 23:13:10 +0200

    cloud fade_out + fix an gameworld

--- src/LudumDare46/Assets/Scripts/VirusCloud.cs
+++ src/LudumDare46/Assets/Scripts/VirusCloud.cs
@@ -6,14 +6,23 @@ public class VirusCloud : MonoBehaviour
 {
     public float deleteAfter = 2f;
 
+    private Animator animator;
+    private Virus virus;
+
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
+        virus = GetComponent<Virus>();
         StartCoroutine("DeleteObj");
     }
 
     IEnumerator DeleteObj(){
+
         yield return new WaitForSeconds(deleteAfter);
+        virus.setVirusActive(false);
+        animator.SetTrigger("fadeOut");
+        yield return new WaitForSeconds(1);
         Destroy(this.gameObject);
     }
 }
